1c633eaa7aacf34da025f2a1ac8a5d7654742f7e
Hector Romero <hectorromerodev@gmail.com>
2021-04-27 16:16:57 -0700

    UPD APP module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { CreateMessageComponent } from './create-message/create-message.component';
 
+// NGXS dependencies
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+import { MessagesState } from './store/messages/message.state';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateMessageComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgxsModule.forRoot([
+      MessagesState
+    ], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
     AppRoutingModule
   ],
   providers: [],
